81b1f638a75617fd1441b718951b4b3eae7a2f3d
Marc Nguyen <nguyen_marc@live.fr>
2021-11-29 23:30:22 +0100

    feat: use custom cache path

--- run.sh
+++ run.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
+mkdir -p /tmp/gentoo
+chmod 777 /tmp/gentoo
+chown 2001:2001 /tmp/gentoo
 
 docker run -it --rm --name gentoo-builder \
-	-v /mnt/nvmesh/gentoo-prefix:/mnt/nvmesh/gentoo-prefix \
+	-v /opt/gentoo:/opt/gentoo \
+	-v /tmp/gentoo:/tmp/gentoo \
+	-e PORTAGE_TMPDIR=/tmp/gentoo \
 	gentoo-builder
 
