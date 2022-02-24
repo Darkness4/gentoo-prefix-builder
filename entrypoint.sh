2a7b57b26439fa4afcaf509e0c75112dcc230b6a
Nguyen Marc <nguyen_marc@live.fr>
2022-02-24 20:04:59 +0100

    feat: add mirror first
    Signed-off-by: Nguyen Marc <nguyen_marc@live.fr>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,24 +1,26 @@
 #!/bin/bash
 
 if [ ! -e "${EPREFIX}/.stage1-finished" ]; then
-  STOP_BOOTSTRAP_AFTER=stage1 LATEST_TREE_YES=1 STABLE_PREFIX="${STABLE_PREFIX}" MAKEOPTS="${MAKEOPTS}" /usr/bin/bootstrap-prefix.sh "${EPREFIX}" noninteractive
-
+  mkdir -p "${EPREFIX}/etc/portage/"
   cat <<EOF >>"${EPREFIX}/etc/portage/repos.conf"
 [gentoo]
 sync-type = rsync
 sync-uri = ${REPOS_MIRRORS}
 EOF
 
+  mkdir -p "${EPREFIX}/tmp/etc/portage/"
   cat <<EOF >>"${EPREFIX}/tmp/etc/portage/repos.conf"
 [gentoo]
 sync-type = rsync
 sync-uri = ${REPOS_MIRRORS}
 EOF
 
+  mkdir -p "${EPREFIX}/opt/gentoo/etc/portage/make.conf/"
   cat <<EOF >/opt/gentoo/etc/portage/make.conf/0101_mirrors_make.conf
 GENTOO_MIRRORS="${GENTOO_MIRRORS}"
 EOF
 
+  mkdir -p "${EPREFIX}/opt/gentoo/etc/portage/make.conf/"
   cat <<EOF >/opt/gentoo/tmp/etc/portage/make.conf/0101_mirrors_make.conf
 GENTOO_MIRRORS="${GENTOO_MIRRORS}"
 EOF
